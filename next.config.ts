1a7846660aea694c73c4311665a0404e82ac678f
Shu Otaku <senhordosubmundo0@gmail.com>
2025-06-26 04:57:43 +0000

    The NextJS app server is having trouble starting. Please identify what c

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,5 @@
 module.exports = {
   /* config options here */
-  typescript: {
-    ignoreBuildErrors: true,
-  },
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
   images: {
     remotePatterns: [
       {
